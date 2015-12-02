98a7d6b3245d9eff8109a230c58d01176d83b885
osvaldo306 <osvaldorojas306@gmail.com>
2015-12-02 22:27:14 +0100

    extra_1

---
+++ cadena.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+	char cadena [10];
+
+	scanf("%s",cadena);
+	printf("%s",cadena);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ extra_1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+  int x,y,z,i,a;
+
+  x=0;
+  y=1;
+  printf("Inserta el numero que desees \n");
+ 
+ scanf("%d",&a);
+  printf("0\n1\n",z);
+ 
+  for (i=1;i<=a;i++)
+  {
+      z=x+y;
+      printf("%d\n",z);
+      x=y;
+      y=z;
+  }
+  
+ 
+  
+}
+
+
